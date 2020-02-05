12ad1fe50c4e458d617cf168084d2117852dc3b0
Yogita.Dhanwate <Yogita.Dhanwate@harbingergroup.com>
2020-02-05 10:45:39 +0530

    starting with command line arguments

--- notes-app/app.js
+++ notes-app/app.js
@@ -6,4 +6,8 @@ const chalk = require('chalk');
 getNotes();
 createNotes();
 
+console.log("---command line argument 1",process.argv[0]);   //node path
+console.log("---command line argument 2",process.argv[1]);   //current running file path
+console.log("---command line argument 3",process.argv[2]);   //command line arguments
+
 console.log(chalk.red.bold('Erro!'));
