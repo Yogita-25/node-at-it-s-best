221d3a179eee616631a7c8a8fba72ad5125bcac9
Yogita.Dhanwate <Yogita.Dhanwate@harbingergroup.com>
2020-03-11 15:47:22 +0530

    using arrow function

--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -1,7 +1,11 @@
 const event = {
     name: "Swara's wedding",
-    printGuestList: function () {
+    guestList: ['kiran', 'Shraddha', 'Swara', 'Sulabha'],
+    printGuestList() {
         console.log("Guest list for", this.name);
+        this.guestList.forEach((guest) => {
+            console.log(guest, " is atending", this.name);
+        })
     }
 }
 
