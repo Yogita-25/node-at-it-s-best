64afe58bb5a4d249b0f66c913205ea46817c6a45
Yogita-25 <dyogita04@gmail.com>
2020-03-18 12:39:02 +0530

    Understanding of Call stack, callback queue and event loop

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,4 +4,18 @@ setTimeout(() => {
     console.log("Print this after 2000 mili seconds i.e 2 seconds");
 }, 2000);
 
+setTimeout(() => {
+    console.log("Print this after 0 seconds");
+}, 0);
+
 console.log("Stop");
+
+// *****************OUTPUT
+// Starting
+// Stop
+// Print this after 0 seconds
+// Print this after 2000 mili seconds i.e 2 seconds
+// ----------------------------------------------
+
+// This "Print this after 0 seconds" is printed after this "Stop"
+// because "Event Loop" does not allow to run "Callback Queue" untill "Call Stack" is empty
