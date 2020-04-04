49ea0b3ba29e53824a50f39851866cdee390b2a9
Yogita Dhanwate <dyogita04@gmail.com>
2020-04-04 21:36:54 +0530

    starting with mongodb

---
+++ task-manager/mongodb.js
@@ -0,0 +1,14 @@
+//crud operations
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const database = 'task-manager';
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error) {
+        return console.log("Unable to connect to database");
+    }
+    console.log("Connected successfully");
+})
