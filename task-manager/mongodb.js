4e10835a865cd93af7b2b351e2f6c838be4bd271
Yogita Dhanwate <dyogita04@gmail.com>
2020-04-05 00:29:48 +0530

    creating ObjectId explicitly

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,29 +1,45 @@
 //crud operations
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectId } = require('mongodb');;
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const database = 'task-manager';
+
+const id = new ObjectId();  
+console.log(id.id.length);
+console.log(id.toHexString().length);
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
         return console.log("Unable to connect to database", error);
     }
     const db = client.db(database);
-    db.collection('users').insertMany([
-        {
-            name : 'kiran',
-            age :12
-        },
-        {
-            name : 'manik',
-            age : 27
-        }
-    ],(error,result)=>{
-        if(error){
-            return console.log("Not able to insert documents");
-        }
-        console.log(result.ops);
-    })
+    // db.collection('users').insertOne(
+    //     {
+            
+    //         name : 'kiran',
+    //         age :12
+    //     }
+    // ,(error,result)=>{
+    //     if(error){
+    //         return console.log("Not able to insert documents");
+    //     }
+    //     console.log(result.ops);
+    // })
+
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'Learning node from beginning',
+    //         completed: false
+    //     },
+    //     {
+    //         description: "Complete Git",
+    //         completed: true
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log("Not able to insert documents");
+    //     }
+    //     console.log(result.ops);
+    // })
 })
