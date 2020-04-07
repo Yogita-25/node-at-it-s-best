d79fcda227697aa48e37f59f93b9977f5d08aedd
Yogita Dhanwate <dyogita04@gmail.com>
2020-04-07 14:01:08 +0530

    understanding data validation and sanitization

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,45 +1,65 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
-    useNewUrlParser: true, useCreateIndex: true            //to create index to access data faster
+    useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true            //to create index to access data faster
 });
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim:true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim : true,
+        lowercase : true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Please enter valid email");
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default : 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number');
+            }
+        }
     }
 });
 
-// const newUser = new User({
-//     name: 'Manik',
-//     age: 'Malhotra'
+const newUser = new User({
+    name: '  Yog ita   ',
+    email :'   dyogita@gmail.com'
+});
+
+newUser.save().then((result) => {
+    console.log(result);
+}).catch((error) => {
+    console.log('Error!', error);
+});
+
+// const Task = mongoose.model('Task', {
+//     description: {
+//         type: String
+//     },
+//     completed: {
+//         type: Boolean
+//     }
 // });
 
-// newUser.save().then((result) => {
+// const newTask = new Task({
+//     description: 'Learning node from Udemy',
+//     completed: false
+// });
+
+// newTask.save().then((result) => {
 //     console.log(result);
 // }).catch((error) => {
-//     console.log('Error!', error);
-// });
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    }
-});
-
-const newTask = new Task({
-    description : 'Learning node from Udemy',
-    completed : false
-});
-
-newTask.save().then((result)=>{
-    console.log(result);
-}).catch((error)=>{
-    console.log("Error!",error);
-})
+//     console.log("Error!", error);
+// })
