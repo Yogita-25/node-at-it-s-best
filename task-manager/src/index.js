cbdf602c34e434eb6ab74e0edc6587382a56bd72
Yogita Dhanwate <dyogita04@gmail.com>
2020-04-27 20:29:52 +0530

    creating task

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -2,21 +2,31 @@ const express = require('express');
 require('./db/mongoose');
 
 const User = require('./models/user');
+const Task = require('./models/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());     //add this otherwise you would get req.body undefined
 
-
 app.post('/users',(req,res)=>{
       console.log("---",req.body);
       const user= new User(req.body);
       user.save().then(()=>{
-      res.send(user);
+      res.status(201).send(user);
     }).catch((e)=>{
       res.status(400).send(e);             //need to add this before sending data
     });
 });
+
+app.post('/tasks',(req,res)=>{
+    console.log("====",req.body);
+    const task = new Task(req.body);
+    task.save().then(()=>{
+        res.status(201).send(task);
+    }).catch((e)=>{
+        res.status(400).send(e);
+    })
+})
 
 app.listen(port ,()=>{
     console.log('Server is up on port ',port);
