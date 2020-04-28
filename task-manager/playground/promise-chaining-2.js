d3d5c6a060c13528c0e923bc196b45bd173d4fda
Yogita Dhanwate <dyogita04@gmail.com>
2020-04-28 15:11:34 +0530

    async await part2

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,11 +1,24 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findOneAndRemove({_id:'5ea6f34965895d344813e0c8'}).then((task)=>{
-    console.log("Removed ==",task);
-    return Task.countDocuments({completed : false})
-}).then((result)=>{
-    console.log(result);
+// Task.findOneAndRemove({_id:'5ea6f34965895d344813e0c8'}).then((task)=>{
+//     console.log("Removed ==",task);
+//     return Task.countDocuments({completed : false});
+// }).then((result)=>{
+//     console.log(result);
+// }).catch((e)=>{
+//     console.log(e);
+// })
+
+const deleteTaskAndCount = async(id)=>{
+   const task = await Task.findOneAndRemove(id);
+   console.log("Deleted task==",task);
+   const count = await Task.countDocuments({completed : false});
+   return count ;
+}
+
+deleteTaskAndCount('5ea6f627093a96342432be93').then((count)=>{
+   console.log("Count==",count);
 }).catch((e)=>{
-    console.log(e);
+    console.log("e===",e);
 })
--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,11 +1,24 @@
 require('../src/db/mongoose');
 const User = require('../src/models/user');
 
-User.findByIdAndUpdate('5ea6f57e093a96342432be92',{age :26}).then((user)=>{
-    console.log("result==",user);
-    return User.countDocuments({age : 26});
-}).then((result)=>{
-    console.log(result);
-}).catch((e)=>{
-    console.log(e);
-})
+// User.findByIdAndUpdate('5ea6f57e093a96342432be92',{age :26}).then((user)=>{
+//     console.log("result==",user);
+//     return User.countDocuments({age : 26});
+// }).then((result)=>{
+//     console.log(result);
+// }).catch((e)=>{
+//     console.log(e);
+// })
+
+const updateAgeAndCount = async(id,age)=>{
+    const user =await User.findByIdAndUpdate(id,{age});
+    console.log("updated user==>",user);
+    const count = await User.countDocuments({age});
+    return count;
+}
+
+updateAgeAndCount('5ea6ec1de6788202f0edf562',22).then((result)=>{
+    console.log("result",result);
+    }).catch((e)=>{
+    console.log("e===",e);
+});
