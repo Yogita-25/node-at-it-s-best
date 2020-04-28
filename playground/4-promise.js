9a7988ed8774931122eef0b0322b86f1ea4c0274
Yogita Dhanwate <dyogita04@gmail.com>
2020-04-28 10:24:55 +0530

    understanding of promise chaining

--- playground/4-promise.js
+++ playground/4-promise.js
@@ -6,13 +6,22 @@ const add = (a,b)=>{
     })
 }
 
-add(4,5).then((sum)=>{
-  console.log("Sum==",sum);
-  add(sum,2).then((result)=>{
-     console.log("2nd ==",result);
-  }).catch((e)=>{
-     console.log("2===error",e);
-  })
+// add(4,5).then((sum)=>{
+//   console.log("Sum==",sum);
+//   add(sum,2).then((result)=>{
+//      console.log("2nd ==",result);
+//   }).catch((e)=>{
+//      console.log("2===error",e);
+//   })
+// }).catch((e)=>{
+//   console.log("Error==",e);
+// })
+
+add(1,2).then((sum)=>{
+   console.log(sum);
+   return add(sum,3);             //you can return a promise from then call of promise to nest another then call with it
+}).then((sum2)=>{
+    console.log(sum2);
 }).catch((e)=>{
-  console.log("Error==",e);
+    console.log(e);
 })
