74f81b41ce1ba03ddda765454ab6248f465b2cbc
Yogita.Dhanwate <Yogita.Dhanwate@harbingergroup.com>
2020-05-03 20:51:58 +0530

    adding authentication to task endponts and cascade delete tasks

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('../models/task');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -92,6 +93,13 @@ userSchema.pre('save', async function (next) {
     }
     next();
 });
+
+//delete user tasks when the user is deleted
+userSchema.pre('remove', async function (next) {
+    const user = this;
+    await Task.deleteMany({createdBy: user._id});
+    next();
+})
 
 const User = mongoose.model('User', userSchema);
 
--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -3,18 +3,22 @@ const Task = require('../models/task');
 const router = new express.Router();
 const auth = require('../middleware/auth');
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
     try {
-        const tasks = await Task.find();
-        res.send(tasks);
+        // const tasks = await Task.find({createdBy: req.user._id });   OR
+        await req.user.populate('tasks').execPopulate();
+        res.send(req.user.tasks);
     } catch (e) {
         res.status(500).send(e);
     }
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
+    const id = req.params.id;
     try {
-        const task = await Task.findById({ _id: req.params.id });
+        // const task = await Task.findById({ _id: req.params.id });
+        const task = await Task.findOne({ _id: id, createdBy: req.user._id });
+
         if (!task) {
             return res.status(404).send();
         }
@@ -24,11 +28,11 @@ router.get('/tasks/:id', async (req, res) => {
     }
 })
 
-router.post('/tasks',auth, async (req, res) => {
+router.post('/tasks', auth, async (req, res) => {
     req.body.createdBy
     const task = new Task({
         ...req.body,
-        createdBy:req.user._id
+        createdBy: req.user._id
     });
     try {
         await task.save()
@@ -38,7 +42,7 @@ router.post('/tasks',auth, async (req, res) => {
     }
 })
 
-router.patch('/task/:id', async (req, res) => {
+router.patch('/task/:id',auth, async (req, res) => {
     try {
         const updates = Object.keys(req.body);   ///returns array of keys
         const allowedUpdates = ['description', 'completed'];
@@ -46,23 +50,25 @@ router.patch('/task/:id', async (req, res) => {
         if (!isValidOperation) {
             return res.status(400).send({ error: 'invalid updates!' });
         }
-        //const task = await Task.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true, runValidators: true });
-        const task = await Task.findById(req.params.id);
-        updates.forEach((update) => {
-            task[update] = req.body[update];
-        })
+       
+        const task = await Task.findOne({_id:req.params.id, createdBy: req.user._id});
+      
         if (!task) {
             return res.status(404).send();
         }
+        updates.forEach((update) => {
+            task[update] = req.body[update];
+        })
+        await task.save();
         res.send(task);
     } catch (e) {
         res.status(500).send(e);
     }
 })
 
-router.delete('/task/:id', async (req, res) => {
+router.delete('/task/:id',auth, async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(req.params.id);
+        const task = await Task.findOneAndDelete({_id:req.params.id,createdBy: req.user._id});
         if (!task) {
             return res.status(400).send();
         }
