b4ba161ea172bbe97db6a4b7cc43530261ce453c
Yogita.Dhanwate <Yogita.Dhanwate@harbingergroup.com>
2020-05-17 09:14:11 +0530

    validating upload image for userr profile

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -81,7 +81,17 @@ router.delete('/user/me', auth, async (req, res) => {    //delete your own accou
 });
 
 const upload = multer({
-    dest: 'avatars'
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000    //number in bytes
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image'));
+        }
+
+        cb(undefined, true);
+    }
 });
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
