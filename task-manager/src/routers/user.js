15c886aaf4b9364e7ecb550166810d82c5863e03
Yogita.Dhanwate <Yogita.Dhanwate@harbingergroup.com>
2020-05-17 08:38:41 +0530

    adding support for filerupload - multer

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 const router = new express.Router();
+const multer = require('multer');
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
@@ -68,7 +69,7 @@ router.patch('/user/me', auth, async (req, res) => {
     } catch (e) {
         res.status(500).send(e);
     }
-})
+});
 
 router.delete('/user/me', auth, async (req, res) => {    //delete your own account
     try {
@@ -79,5 +80,12 @@ router.delete('/user/me', auth, async (req, res) => {    //delete your own accou
     }
 });
 
+const upload = multer({
+    dest: 'avatars'
+});
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+    res.send();
+});
 
 module.exports = router;
