5c57c9cc07a51cff739adee4d3c7545e7378212d
Yogita.Dhanwate <Yogita.Dhanwate@harbingergroup.com>
2020-05-24 14:41:36 +0530

    adding test cases for default tip

--- task-manager/test/math.test.js
+++ task-manager/test/math.test.js
@@ -2,7 +2,14 @@ const { calculateTip } = require('../src/math');
 
 test('Should calculate total with tip', () => {
     const total = calculateTip(10, .3);
-    if(total != 13){
-        throw new Error('Total tip should be 13. Got ',total);
-    }
+
+    expect(total).toBe(13);
+    // if(total != 13){
+    //     throw new Error('Total tip should be 13. Got ',total);
+    // }
 });
+
+test('should calculate total with default tip',()=>{
+    const total = calculateTip(10);
+    expect(total).toBe(12.25);
+})
